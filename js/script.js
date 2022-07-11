2e5a2885efb319f3cd4aa7dbf9afb89a22dcc421
Steveh77 <stederosa76@gmail.com>
2022-07-11 17:53:54 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const root = new Vue ({
         firstName:"Stefano",
         lastName: "De Rosa",
         classRoom: "CLASSE 68",
+        logo:"https://yt3.ggpht.com/ytc/AKedOLTO2XVhtyMr24Dnz6QJ-Lsj_05XHim-qMoF6PRc=s900-c-k-c0x00ffffff-no-rj"
         
     },
     methods:{
